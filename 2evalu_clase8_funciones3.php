6d0a13c7fbcea6efb60287e1de13d224d520b623
meladsz <mela05.ds@gmail.com>
2026-01-23 12:33:03 +0100

    Create 2evalu_clase8_funciones3.php
    Add initial PHP file for a simple calculator program.

---
+++ 2evalu_clase8_funciones3.php
@@ -0,0 +1,5 @@
+<?php
+// hacer que la longitud de los asteriscos sea igual que el mensaje 
+// programa que sea una pequeña calculadora, que reciba una operacion de 2 numeros sea multi, rest, sum,.. 
+
+?>
