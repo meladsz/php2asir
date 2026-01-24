6dcde01ebb6db0a283fa8671e65b580c420a6f1a
meladsz <mela05.ds@gmail.com>
2026-01-24 15:10:53 +0100

    Update 2evalu_clase8_funciones3.php

--- 2evalu_clase8_funciones3.php
+++ 2evalu_clase8_funciones3.php
@@ -1,5 +1,20 @@
 <?php
 // hacer que la longitud de los asteriscos sea igual que el mensaje 
+
+function mostrarAsteriscos($mensaje) {
+    $longitud = strlen($mensaje);
+
+    for ($i = 0; $i < $longitud; $i++) {
+        echo "*";
+    }
+}
+
+// programa principal
+$mensaje = "Hola mundo";
+
+echo $mensaje . "<br>";
+mostrarAsteriscos($mensaje);
+
 // programa que sea una pequeña calculadora, que reciba una operacion de 2 numeros sea multi, rest, sum,.. 
 
 ?>
