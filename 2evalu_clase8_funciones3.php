c57305df69abe0c5ffe1c1ba5183ef7c005d0180
meladsz <mela05.ds@gmail.com>
2026-01-25 14:12:23 +0100

    Update 2evalu_clase8_funciones3.php

--- 2evalu_clase8_funciones3.php
+++ 2evalu_clase8_funciones3.php
@@ -16,5 +16,26 @@ echo $mensaje . "<br>";
 mostrarAsteriscos($mensaje);
 
 // programa que sea una pequeña calculadora, que reciba una operacion de 2 numeros sea multi, rest, sum,.. 
+function calculadora($num1, $num2, $operacion) {
+    if ($operacion == "+") {
+        return $num1 + $num2;
+    } elseif ($operacion == "-") {
+        return $num1 - $num2;
+    } elseif ($operacion == "*") {
+        return $num1 * $num2;
+    } elseif ($operacion == "/") {
+        return $num1 / $num2;
+    }
+}
+
+// programa principal
+$num1 = rand(1,10);
+$num2 = rand(1,10);
+$operacion = "*";
+
+echo "Número 1: " . $num1 . "<br>";
+echo "Número 2: " . $num2 . "<br>";
+echo "Operación: " . $operacion . "<br>";
+echo "Resultado: " . calculadora($num1, $num2, $operacion);
 
 ?>
