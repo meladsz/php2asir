e7cdf9392746387e1b7d86886ed24c0c7c0d8168
meladsz <mela05.ds@gmail.com>
2026-01-29 21:18:17 +0100

    Update 2evalu_clase9_funciones4.php

--- 2evalu_clase9_funciones4.php
+++ 2evalu_clase9_funciones4.php
@@ -5,4 +5,31 @@
 $aux = $n1;
 $num1=$num2;
 $num2=$aux; */
+function numerosNoRepetidos($tamano) {     
+    $numeros = array();                         
+
+    while (count($numeros) < $tamano) {         
+        $aleatorio = rand(1, 10);               
+        $repetido = false;                  
+
+        for ($i = 0; $i < count($numeros); $i++) { 
+            if ($numeros[$i] == $aleatorio) {   
+                $repetido = true;  
+            }
+        }
+
+        if ($repetido == false) {               
+            $numeros[] = $aleatorio;             
+        }
+    }
+
+    return $numeros;                           
+}
+// programa principal 
+$vector = numerosNoRepetidos(5);         
+
+for ($i = 0; $i < count($vector); $i++) {
+    echo "Número: " . $vector[$i] . "<br>"; 
+}
+
 ?>
