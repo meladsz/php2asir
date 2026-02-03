b6648a9cc06e2b13fca8f1d41db394024caa514d
meladsz <mela05.ds@gmail.com>
2026-02-03 21:05:11 +0100

    Update EstudiandoPapel.php

--- EstudiandoPapel.php
+++ EstudiandoPapel.php
@@ -1,9 +1,8 @@
 <?php
-
+// nivel 2 del dia 6 de EDU
 // array de mensajes
 $mensajes = ["Hola peña", "Buen día", "Qué tal?"];
 
-// función que muestra saludo
 function saludo($texto) {
     echo "*****<br>";
     echo $texto . "<br>";
@@ -11,10 +10,23 @@ function saludo($texto) {
 }
 
 // programa principal
-$i = rand(0, 2); // genera número aleatorio entre 0 y 2
+$i = rand(0, 2); 
 echo "Salió el mensaje $i <br>";
 
 // mostrar el mensaje aleatorio
 saludo($mensajes[$i]);
 
 ?>
+<?php
+// nivel -2 del dia 7 de EDU
+// función que muestra un número
+function mostrarNumero($num) {
+    echo "El número es: " . $num . "<br>";
+}
+
+// programa principal
+$numero = rand(1, 10); // número aleatorio entre 1 y 10
+mostrarNumero($numero);
+
+?>
+
